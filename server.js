1cba48b9428af3a6d528b59a96a15aa8be74ea73
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-14 11:27:51 -0700

    Config for Vercel

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 })
 
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+    });
+
+module.exports = app;
